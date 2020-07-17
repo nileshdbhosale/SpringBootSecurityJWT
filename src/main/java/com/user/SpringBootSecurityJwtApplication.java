468872d71a3c5b1925b99b07329d9f86dbfaf790
Nilesh Bhosale <Nilesh@Nileshs-MacBook-Air.local>
2020-07-16 22:36:21 -0400

    Initial Commit

---
+++ src/main/java/com/user/SpringBootSecurityJwtApplication.java
@@ -0,0 +1,14 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+		System.out.println("Welcome to Spring Secuirty JWT example !");
+	}
+
+}
